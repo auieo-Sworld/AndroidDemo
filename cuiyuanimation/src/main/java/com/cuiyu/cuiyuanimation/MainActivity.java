7f538c6e4f72eebbd43e8f9dba11080413588dbf
CuiYu1997 <2974583593@qq.com>
2020-05-18 21:35:12 +0800

    add animationset that include alpha rotate scale and translate anim

--- cuiyuanimation/src/main/java/com/cuiyu/cuiyuanimation/MainActivity.java
+++ cuiyuanimation/src/main/java/com/cuiyu/cuiyuanimation/MainActivity.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -15,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
   private ImageView framAnim;
     private AnimationDrawable animationDrawable;
     private RelativeLayout rlQqtrembleAni;
-    private Button btnRotate,btnTranslate,btnScale,btnAlpha;
+    private Button btnRotate,btnTranslate,btnScale,btnAlpha,btnAll;
     private boolean flag = true;
     private ImageView iv;
     @Override
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         btnTranslate = findViewById(R.id.btn_two);
         btnScale = findViewById(R.id.btn_three);
         btnAlpha = findViewById(R.id.btn_four);
+        btnAll = findViewById(R.id.btn_five);
         iv = findViewById(R.id.iv);
         final Animation rotate = AnimationUtils.loadAnimation(this,
                 R.anim.anim_rotate);
@@ -73,5 +79,48 @@ public class MainActivity extends AppCompatActivity {
               rlQqtrembleAni.startAnimation(tremble);
             }
         });
+        btnAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                beginAnimation();
+            }
+        });
+    }
+    private void beginAnimation(){
+        AnimationSet aniSet = new AnimationSet(false);
+
+        AlphaAnimation alpha = new AlphaAnimation(0,1);
+        alpha.setDuration(4000);
+        aniSet.addAnimation(alpha);
+
+        RotateAnimation rotateAnimation = new RotateAnimation(0,360,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f,
+                RotateAnimation.RELATIVE_TO_SELF,0.5f);
+        rotateAnimation.setDuration(4000);
+
+        ScaleAnimation scaleAnimation = new ScaleAnimation(1.5f,0.5f,1.5f,0.5f);
+        scaleAnimation.setDuration(4000);
+        aniSet.addAnimation(scaleAnimation);
+
+        TranslateAnimation translateAnimation = new TranslateAnimation(0,160,0,240);
+        translateAnimation.setDuration(4000);
+        aniSet.addAnimation(translateAnimation);
+        aniSet.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        iv.startAnimation(aniSet);
     }
 }
